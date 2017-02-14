6e72532531c0923b13b75288708b9db6d3f31c89
liness <wozhisuifeng@gmail.com>
2017-02-14 17:27:31 +0800

    Add files via upload

---
+++ stuphotos.py
@@ -0,0 +1,48 @@
+import urllib2
+
+url = 'http://61.181.145.1:88/Stuphotos/'
+banji = []
+zhuanye = []
+for a in range(0,8):
+    for b in range(1,10):
+        banji.append(str(a) + str(b))
+for c in range(0,8):
+    for d in range(1,5):
+        zhuanye.append(str(c) + str(d))
+
+def getFile(url):
+    file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        f.write(buffer)
+    f.close()
+    print "Sucessful to download" + " " + file_name
+
+for year in range(12, 13):
+    for xh in zhuanye:
+        for nj in banji:
+            for i in range(1, 36):
+                if i < 10:
+                    xuehao = str(year) + str(xh) + str(nj) + '0' + str(i)
+                    student_url = url + xuehao+'.jpg'
+                    print(student_url)
+                    try:
+                        getFile(student_url)
+                    except IOError:
+                        pass
+                else:
+                    xuehao = str(year) + str(xh) + str(nj) + str(i)
+                    student_url = url + xuehao+'.jpg'
+                    print(student_url)
+                    try:
+                        getFile(student_url)
+                    except IOError:
+                        pass
+
