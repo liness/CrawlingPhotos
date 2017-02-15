6c3d211abd70f6f8b0f8b6e9a6ff9f71a7d210cc
liness <wozhisuifeng@gmail.com>
2017-02-15 14:21:49 +0800

    Update stuphotos.py

--- stuphotos.py
+++ stuphotos.py
@@ -1,13 +1,13 @@
 import urllib2
-#爬取学籍照
-url = 'http://***'
+
+url = 'http://*'
 banji = []
 zhuanye = []
-for a in range(0,8):
+for a in range(0,10):
     for b in range(1,10):
         banji.append(str(a) + str(b))
-for c in range(0,8):
-    for d in range(1,5):
+for c in range(0,10):
+    for d in range(1,3):
         zhuanye.append(str(c) + str(d))
 
 def getFile(url):
@@ -25,10 +25,10 @@ def getFile(url):
     f.close()
     print "Sucessful to download" + " " + file_name
 
-for year in range(12, 13):
+for year in range(14, 15):
     for xh in zhuanye:
         for nj in banji:
-            for i in range(1, 36):
+            for i in range(1, 40):
                 if i < 10:
                     xuehao = str(year) + str(xh) + str(nj) + '0' + str(i)
                     student_url = url + xuehao+'.jpg'
